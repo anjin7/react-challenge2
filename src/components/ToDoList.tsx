b5e47981c8dedb6bbf05d46c0fc1fca15c747741
_An_Jin_ <aji0719@gmail.com>
2022-04-13 16:02:35 +0900

    정리

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,21 +2,98 @@ import { useRecoilValue } from "recoil";
 import { toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const toDos = useRecoilValue(toDoState);
+//   console.log(toDos);
+//   return (
+//     <div>
+//       <h1>To Dos</h1>
+//       <hr />
+//       <CreateToDo />
+//       <ul>
+//         {toDos.map((toDo) => (
+//           <ToDo key={toDo.id} {...toDo} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// export default ToDoList;
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+};
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: any) => {
+    console.log(data);
+  };
   return (
     <div>
-      <h1>To Dos</h1>
-      <hr />
-      <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", { required: "write here" })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: "write here", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message}</span>
+        <button>Add</button>
+      </form>
     </div>
   );
 }
+
 export default ToDoList;
