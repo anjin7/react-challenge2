b965101c53957b0cf99a3b37530dd29cae0e1a06
_An_Jin_ <aji0719@gmail.com>
2022-05-15 15:54:39 +0900

    bgColor

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme } from "styled-components";
 export const darkTheme: DefaultTheme = {
-  bgColor: "#3F8CF2",
+  bgColor: "#2f3640",
   boardColor: "#DADFE9",
   cardColor: "white",
 };
